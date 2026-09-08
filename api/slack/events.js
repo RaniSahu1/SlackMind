0b98d24ce9b658390cd5bcb53d772ff9a865def0
ranisahu95590@gmail,com <ranisahu95590@gmail.com>
2026-09-08 13:49:43 +0530

    Add Vercel Slack endpoint

--- api/slack/events.js
+++ api/slack/events.js
@@ -1,8 +1,21 @@
 import { createHandler } from "@vercel/slack-bolt";
 import slackApp from "../../src/config/slack.js";
 import receiver from "../../src/config/vercelReceiver.js";
+import "../../src/slack/events.js";
+import { connectRedis } from "../../src/config/redis.js";
 
-export const POST = createHandler(
-  slackApp,
-  receiver
-);
+let redisConnected = false;
+
+export const POST = async (request) => {
+  if (!redisConnected) {
+    await connectRedis();
+    redisConnected = true;
+  }
+
+  const handler = createHandler(
+    slackApp,
+    receiver
+  );
+
+  return handler(request);
+};
